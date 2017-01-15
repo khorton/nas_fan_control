65ad32818edc44f0dd5bc2a2b2b2aa458c160235
Kevin Horton <kevin01@kilohotel.com>
2017-01-15 09:11:01 -0500

    add ipmitool location

--- fan_check.pl
+++ fan_check.pl
@@ -1,5 +1,7 @@
 #! /usr/local/bin/perl
 
+$ipmitool = "/usr/local/bin/ipmitool";
+
 $n = 80;
 $ave_fan_speed = ( get_fan_speed('FAN1') + get_fan_speed('FAN2') + get_fan_speed('FAN4') + get_fan_speed('FAN5') + get_fan_speed('FANA') + get_fan_speed('FANB') + get_fan_speed('FANC') ) / 7;
 print $ave_fan_speed;
