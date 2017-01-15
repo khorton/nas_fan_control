2a19ad7ec5d9100c0ab15af2516bbace44cae15e
Kevin Horton <kevin01@kilohotel.com>
2017-01-15 09:34:06 -0500

    fix print format

--- fan_check.pl
+++ fan_check.pl
@@ -9,7 +9,7 @@ while( $n > 24){
     set_fan_zone_duty_cycle(1,$n);
     sleep 10;
     $ave_fan_speed = ( get_fan_speed('FAN1') + get_fan_speed('FAN2') + get_fan_speed('FAN4') + get_fan_speed('FAN5') + get_fan_speed('FANA') + get_fan_speed('FANB') + get_fan_speed('FANC') ) / 7;
-    print $n, $ave_fan_speed;
+    print "$n   $ave_fan_speed\n";
     $n -+1;
 }
 
