be026f1826b53a4d265be0548786e82d36486428
Kevin Horton <kevin01@kilohotel.com>
2017-01-15 09:31:36 -0500

    ready to test

--- fan_check.pl
+++ fan_check.pl
@@ -3,8 +3,15 @@
 $ipmitool = "/usr/local/bin/ipmitool";
 
 $n = 80;
-$ave_fan_speed = ( get_fan_speed('FAN1') + get_fan_speed('FAN2') + get_fan_speed('FAN4') + get_fan_speed('FAN5') + get_fan_speed('FANA') + get_fan_speed('FANB') + get_fan_speed('FANC') ) / 7;
-print $ave_fan_speed;
+
+while( n > 24){
+    set_fan_zone_duty_cycle(0,$n);
+    set_fan_zone_duty_cycle(1,$n);
+    sleep 10;
+    $ave_fan_speed = ( get_fan_speed('FAN1') + get_fan_speed('FAN2') + get_fan_speed('FAN4') + get_fan_speed('FAN5') + get_fan_speed('FANA') + get_fan_speed('FANB') + get_fan_speed('FANC') ) / 7;
+    print $n, $ave_fan_speed;
+    $n -+1;
+}
 
 sub get_fan_speed
 {
@@ -19,3 +26,11 @@ sub get_fan_speed
     return $fan_speed;
 }
 
+sub set_fan_zone_duty_cycle
+{
+    my ( $zone, $duty ) = @_;
+    
+    `$ipmitool raw 0x30 0x70 0x66 0x01 $zone $duty`;
+    
+    return;
+}
