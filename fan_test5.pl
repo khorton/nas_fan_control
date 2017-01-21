050a95129a7e9df75e0f9f125fcb5466678d2ae6
Kevin Horton <kevin01@kilohotel.com>
2017-01-21 16:14:25 -0500

    Fix on-screen date output
    also remove commented out code

--- fan_test5.pl
+++ fan_test5.pl
@@ -347,6 +347,7 @@ sub main
     } # inf loop
 }
 
+################################################# SUBS
 sub get_hd_list
 {
     my $disk_list = `camcontrol devlist | grep -v "SSD" | sed 's:.*(::;s:).*::;s:,pass[0-9]*::;s:pass[0-9]*,::' | egrep '^[a]*da[0-9]+\$' | tr '\012' ' '`;
@@ -396,47 +397,6 @@ sub get_hd_temp
     return $max_temp;
 }
 
-# sub get_hd_max_ave_temp
-# # return maximum and average HD temperatures
-# {
-#     my $max_temp = 0;
-#     my $temp_sum = 0;
-#     my $HD_count = 0;
-# 
-#     foreach my $item (@hd_list)
-#     {
-#         my $disk_dev = "/dev/$item";
-#         my $command = "/usr/local/sbin/smartctl -A $disk_dev | grep Temperature_Celsius";
-# 
-#         dprint( 3, "$command\n" );
-# 
-#         my $output = `$command`;
-# 
-#         dprint( 2, "$output");
-# 
-#         my @vals = split(" ", $output);
-# 
-#         # grab 10th item from the output, which is the hard drive temperature (on Seagate NAS HDs)
-#         my $temp = "$vals[9]";
-#         chomp $temp;
-# 
-#         if( $temp )
-#         {
-#             dprint( 1, "$disk_dev: $temp\n");
-#             $temp_sum += $temp;
-#             $HD_count +=1;
-#             $max_temp = $temp if $temp > $max_temp;
-#         }
-#     }
-# 
-#     my $ave_temp = $temp_sum / $HD_count;
-# 
-#     dprint(0, "Average HD Temperature: $ave_temp\n");
-# 
-# 
-#     return ($max_temp, $ave_temp);
-# }
-
 sub get_hd_temps
 # return minimum, maximum, average HD temperatures and array of individual temps
 {
@@ -473,178 +433,6 @@ sub get_hd_temps
 
     return ($min_temp, $max_temp, $ave_temp, @temp_list);
 }
-
-
-###########################
-# verify_fan_speed_levels() 
-# this function verifies a fan zone is high, when it should be high, and low when it should be low.
-# you pass in the cpu_fan_level and the hd_fan_duty (note: level vs duty!). If the hd fan duty is
-# overridden, then you need to pass in the overridden duty.
-#
-# The tricks are that 
-#    1) we need to wait at least 10 seconds after changing a fan level before checking if its
-#        made the change.
-#    2) if we do read the change, and its not right, we should try again, after redoing the change
-#    3) when the BMC has been reset, we can read rubbish... we shouldn't just re-set the BMC in this case
-#       as it should become good, but it also might not become good... in which case we should reset it!
-#    4) if we do reset the BMC, we need to reverify
-#    5) we don't want to re-verify continuously, so if its all good we wait an extra 60 seconds, unless
-#       the fans change in the meantime.
-#
-# to accomplish that, we use a few globals:
-#
-#    last_fan_level_change_time    this is the time when the last fan change was made, and should be updated, 
-#                          whenever a fan change is made. We also updated it each time through the
-#                    verify function so that we will not re-verify until our delay has expired
-#
-#    fan_unreadable_time        this is the time that the fan read failures started, or 0 if there are none
-#                    once the failure has exceeded the bmc_reboot_grace_time threshold, we will reboot.
-#
-#    bmc_fail_count            this is how many times in a row the fan speeds have not been what they should've
-#                    been. If we exceed bmc_fail_threshold, then we reboot the bmc.
-#
-# Configuration globals used:
-#
-#    bmc_fail_threshold        how many times the bmc can have the wrong fan speeds in a row before we reboot.
-#    fan_speed_change_delay        how many seconds we wait until after fan change before verifying, and thence how
-#                    often we verify.
-#    hd/cpu_max_fan_speed        depending on if we want high or low, if the fan speed is over or under, we'll regard
-#                    it as a failure.
-#
-# sub verify_fan_speed_levels
-# {
-#     my( $cpu_fan_level, $hd_fan_duty ) = @_;
-#     # dprint( 4, "verify_fan_speed_levels: cpu_fan_level: $cpu_fan_level, hd_fan_duty: $hd_fan_duty, CPU HD override: $cpu_min_duty_cycle_from_hds\n");
-#     dprint( 4, "verify_fan_speed_levels: cpu_fan_level: $cpu_fan_level, hd_fan_duty: $hd_fan_duty\n");
-# 
-#     my $extra_delay_before_next_check = 0;
-#     
-#     my $temp_time = time - $last_fan_level_change_time;
-#     dprint( 4, "Time since last verify : $temp_time, last change: $last_fan_level_change_time, delay: $fan_speed_change_delay\n");
-#     if( $temp_time > $fan_speed_change_delay )
-#     {
-#         # we've waited for the speed change to take effect.
-#         
-#         my $cpu_fan_speed = get_fan_speed("CPU");
-#         if( $cpu_fan_speed < 0 )
-#         {
-#             dprint(1,"CPU Fan speed unavailable\n" );
-#             $fan_unreadable_time = time if $fan_unreadable_time == 0;
-#         }
-#         
-#         my $hd_fan_speed = get_fan_speed("HD");
-#         if( $hd_fan_speed < 0 )
-#         {
-#             dprint(1,"HD Fan speed unavailable\n" );
-#             $fan_unreadable_time = time if $fan_unreadable_time == 0;
-#         }
-#         
-#         if( $hd_fan_speed < 0 || $cpu_fan_speed < 0 )
-#         {
-#             # one of the fans couldn't be reliably read
-# 
-#             my $temp_time = time - $fan_unreadable_time;
-#             if( $temp_time > $bmc_reboot_grace_time )
-#             {
-#                 #we've waited, and we still can't read fan speed.
-#                 dprint(0, "Fan speeds are unreadable after $bmc_reboot_grace_time seconds, rebooting BMC\n");
-#                 reset_bmc();
-#                 $fan_unreadable_time = 0;
-#             }
-#             else
-#             {
-#                 dprint(2, "Fan speeds are unreadable after $temp_time seconds, will try again\n");    
-#             }        
-#         }
-#         else
-#         {
-#             # we have no been able to read the fan speeds
-# 
-#             my $cpu_fan_is_wrong = 0;
-#             my $hd_fan_is_wrong = 0;    
-#             
-#             #verify cpu fans
-#             if( $cpu_min_duty_cycle_from_hds == 0 )
-#             {
-#                 if( $cpu_fan_level eq "high" && $cpu_fan_speed < $cpu_max_fan_speed )
-#                 {
-#                     dprint(0, "CPU fan speed should be high, but $cpu_fan_speed < $cpu_max_fan_speed.\n");
-#                     $cpu_fan_is_wrong=1;
-#                 }
-#                 elsif( $cpu_fan_level eq "low" && $cpu_fan_speed > $cpu_max_fan_speed )
-#                 {
-#                     dprint(0, "CPU fan speed should be low, but $cpu_fan_speed > $cpu_max_fan_speed.\n");
-#                     $cpu_fan_is_wrong=1;
-#                 }
-#             }
-#             else
-#             {
-#                 if( $cpu_fan_speed < $cpu_hd_fan_speed )
-#                 {
-#                     dprint(0, "CPU fan speed should be controlled by HD temps, but $cpu_fan_speed < $cpu_hd_fan_speed.\n");
-#                     $cpu_fan_is_wrong=1;
-#                 }
-#             }
-#             
-#             #verify hd fans
-#             if( $hd_fan_duty >= $hd_fan_duty_high && $hd_fan_speed < $hd_max_fan_speed )
-#             {
-#                 dprint(0, "HD fan speed should be high, but $hd_fan_speed < $hd_max_fan_speed.\n");
-#                 $hd_fan_is_wrong=1;
-#             }
-#             elsif( $hd_fan_duty <= $hd_fan_duty_low && $hd_fan_speed > $hd_max_fan_speed )
-#             {
-#                 dprint(0, "HD fan speed should be low, but $hd_fan_speed > $hd_max_fan_speed.\n");
-#                 $hd_fan_is_wrong=1;
-#             }
-#             
-#             #verify both fans are good
-#             if( $cpu_fan_is_wrong || $hd_fan_is_wrong )
-#             {
-#                 $bmc_fail_count++;
-#                 
-#                 dprint( 3, "bmc_fail_count:  $bmc_fail_count, bmc_fail_threshold: $bmc_fail_threshold\n");
-#                 if( $bmc_fail_count <= $bmc_fail_threshold )
-#                 {
-#                     #we'll try setting the fan speeds, and giving it another attempt
-#                     dprint(1, "Fan speeds are not where they should be, will try again.\n");
-# 
-#                     set_fan_mode("full");
-# 
-#                     set_fan_zone_level( $cpu_fan_zone, $cpu_fan_level );
-#                     set_fan_zone_duty_cycle( $hd_fan_zone, $hd_fan_duty );
-#                 }
-#                 else
-#                 {
-#                     #time to reset the bmc
-#                     dprint(1, "Fan speeds are still not where they should be after $bmc_fail_count attempts, will reboot BMC.\n");
-#                     set_fan_mode("full");
-#                     reset_bmc();
-#                     $bmc_fail_count = 0;
-#                 }
-#             }
-#             else
-#             {
-#                 #everything is good. We'll sit back for another minute.
-# 
-#                 dprint( 2, "Verified fan levels, CPU: $cpu_fan_speed, HD: $hd_fan_speed. All good.\n" );
-#                 $bmc_fail_count = 0; # we succeeded
-# 
-#                 $extra_delay_before_next_check = 60 - $fan_speed_change_delay; # lets give it a minute since it was good.
-#             }    
-# 
-#                 
-#             #reset our unreadable timer, since we read the fan speeds.
-#             $fan_unreadable_time = 0;
-#                                     
-#         }
-#             
-#         #reset our timer, so that we'll wait before checking again.
-#         $last_fan_level_change_time = time + $extra_delay_before_next_check; #another delay before checking please.
-#     }
-#     
-#     return;
-# }
 
 sub verify_fan_speed_levels
 {
@@ -769,7 +557,6 @@ sub verify_fan_speed_levels
 	return;
 }
 
-################################################# SUBS
 
 # need to pass in last $cpu_fan
 sub control_cpu_fan
@@ -854,12 +641,12 @@ sub calculate_hd_fan_duty_cycle_PID
     return int($hd_duty + 0.5);
 }
 
-# sub build_date_string
-# {
-#     my $datestring = strftime "%F %H:%M:%S", localtime;
-#     
-#     return $datestring;
-# }
+sub build_date_time_string
+{
+    my $datetimestring = strftime "%F %H:%M:%S", localtime;
+    
+    return $datetimestring;
+}
 
 sub build_date_string
 {
@@ -935,7 +722,7 @@ sub dprint
     
     if( $debug > $level ) 
     {
-        my $datestring = build_date_string();
+        my $datestring = build_date_time_string();
         print "$datestring: $output";
     }
 
