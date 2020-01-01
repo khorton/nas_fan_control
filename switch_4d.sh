90dce43a3414482d69a1e9adf3cc7c9efab69693
Kevin Horton <kevin@khavserv.com>
2019-12-31 21:07:05 -0800

    delete unneeded files for borg

--- switch_4d.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-rm  /root/nas_fan_control/PID_fan_control_config.ini
-ln -s /root/nas_fan_control/4d_config.ini /root/nas_fan_control/PID_fan_control_config.ini
-touch /root/nas_fan_control/4d_config.ini
