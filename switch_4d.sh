3273066cf81704e2cd68e34dc2910989b21bfce3
Kevin Horton <kevin01@kilohotel.com>
2020-01-01 18:50:07 -0800

    rename to get rid of Borg specific file names

---
+++ switch_4d.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+rm  /root/nas_fan_control/PID_fan_control_config.ini
+ln -s /root/nas_fan_control/4d_config.ini /root/nas_fan_control/PID_fan_control_config.ini
+touch /root/nas_fan_control/4d_config.ini
--- switch_4d_borg.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-rm  /root/nas_fan_control/PID_fan_control_config.ini
-ln -s /root/nas_fan_control/4d_config_borg.ini /root/nas_fan_control/PID_fan_control_config.ini
-touch /root/nas_fan_control/4d_config_borg.ini
