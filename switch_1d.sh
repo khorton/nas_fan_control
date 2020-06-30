4c220b4a5cb8c4677326d8c70f87ab2b8c84e8f4
Kevin Horton <kevin@khavserv.com>
2020-06-30 06:19:08 -0700

    add script to switch to one disk control

---
+++ switch_1d.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# switch config files so that 2d_config.ini is the active config
+rm  /root/nas_fan_control/PID_fan_control_config.ini
+ln -s /root/nas_fan_control/1d_config.ini /root/nas_fan_control/PID_fan_control_config.ini
+touch /root/nas_fan_control/1d_config.ini
